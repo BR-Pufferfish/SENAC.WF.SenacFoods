f272c253968afae7e020713891785ab3ed902ccc
Marcelo Ballin <puffersenac@gmail.com>
2025-04-24 21:52:25 -0300

    Criado Login e Form Principal

---
+++ SenacFoodsSolucao/SenacFoods/FrmLogin.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SenacFoods
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            panel1 = new Panel();
+            btn_login = new Button();
+            txt_password = new TextBox();
+            txt_username = new TextBox();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btn_login);
+            panel1.Controls.Add(txt_password);
+            panel1.Controls.Add(txt_username);
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(333, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(849, 653);
+            panel1.TabIndex = 0;
+            // 
+            // btn_login
+            // 
+            btn_login.BackColor = SystemColors.ActiveCaptionText;
+            btn_login.FlatAppearance.MouseDownBackColor = SystemColors.ActiveCaption;
+            btn_login.FlatAppearance.MouseOverBackColor = SystemColors.ActiveBorder;
+            btn_login.FlatStyle = FlatStyle.Flat;
+            btn_login.Font = new Font("MS Reference Sans Serif", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_login.ForeColor = SystemColors.Info;
+            btn_login.Location = new Point(333, 339);
+            btn_login.Name = "btn_login";
+            btn_login.Size = new Size(161, 58);
+            btn_login.TabIndex = 2;
+            btn_login.Text = "Entrar";
+            btn_login.UseVisualStyleBackColor = false;
+            // 
+            // txt_password
+            // 
+            txt_password.BackColor = SystemColors.WindowText;
+            txt_password.BorderStyle = BorderStyle.FixedSingle;
+            txt_password.Font = new Font("MS Reference Sans Serif", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txt_password.ForeColor = SystemColors.Info;
+            txt_password.Location = new Point(247, 272);
+            txt_password.Name = "txt_password";
+            txt_password.Size = new Size(311, 35);
+            txt_password.TabIndex = 1;
+            txt_password.Text = "Senha";
+            // 
+            // txt_username
+            // 
+            txt_username.BackColor = SystemColors.WindowText;
+            txt_username.BorderStyle = BorderStyle.FixedSingle;
+            txt_username.Font = new Font("MS Reference Sans Serif", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txt_username.ForeColor = SystemColors.Info;
+            txt_username.Location = new Point(247, 202);
+            txt_username.Name = "txt_username";
+            txt_username.Size = new Size(311, 35);
+            txt_username.TabIndex = 0;
+            txt_username.Text = "Usuário";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.AppWorkspace;
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(310, 653);
+            panel2.TabIndex = 1;
+            // 
+            // FrmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1182, 653);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "FrmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox txt_password;
+        private TextBox txt_username;
+        private Button btn_login;
+    }
+}
---
+++ SenacFoodsSolucao/SenacFoods/FrmPrincipal.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace SenacFoods
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.ForeColor = SystemColors.Info;
+            groupBox1.Location = new Point(12, 129);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1140, 465);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Senac Foods";
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.MouseDownBackColor = Color.Goldenrod;
+            button1.FlatAppearance.MouseOverBackColor = Color.DarkGoldenrod;
+            button1.Font = new Font("Century Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(93, 147);
+            button1.Name = "button1";
+            button1.Size = new Size(166, 147);
+            button1.TabIndex = 0;
+            button1.Text = "Cardápio";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.MouseDownBackColor = Color.Green;
+            button2.FlatAppearance.MouseOverBackColor = Color.ForestGreen;
+            button2.Font = new Font("Century Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ControlText;
+            button2.Location = new Point(361, 147);
+            button2.Name = "button2";
+            button2.Size = new Size(166, 147);
+            button2.TabIndex = 0;
+            button2.Text = "Comanda";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.FlatAppearance.MouseDownBackColor = Color.LightSeaGreen;
+            button3.FlatAppearance.MouseOverBackColor = Color.Teal;
+            button3.Font = new Font("Century Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ControlText;
+            button3.Location = new Point(629, 147);
+            button3.Name = "button3";
+            button3.Size = new Size(166, 147);
+            button3.TabIndex = 0;
+            button3.Text = "Pedido Cozinha";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.FlatAppearance.MouseDownBackColor = Color.DarkRed;
+            button4.FlatAppearance.MouseOverBackColor = Color.Firebrick;
+            button4.Font = new Font("Century Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.ControlText;
+            button4.Location = new Point(894, 147);
+            button4.Name = "button4";
+            button4.Size = new Size(166, 147);
+            button4.TabIndex = 0;
+            button4.Text = "Usuários";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1164, 606);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FrmPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FrmPrincipal";
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button1;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+    }
+}

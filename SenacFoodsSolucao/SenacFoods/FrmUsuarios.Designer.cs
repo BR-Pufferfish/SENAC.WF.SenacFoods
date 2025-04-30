ffc83cfb740646b4993016db9f93dc01ab2bd790
Marcelo Ballin <puffersenac@gmail.com>
2025-04-29 21:27:37 -0300

    Criada a base da base dos forms

--- SenacFoodsSolucao/SenacFoods/FrmUsuarios.Designer.cs
+++ SenacFoodsSolucao/SenacFoods/FrmUsuarios.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmUsuarios));
             btn_close = new PictureBox();
+            groupBox1 = new GroupBox();
+            txt_usuarios = new TextBox();
             ((System.ComponentModel.ISupportInitialize)btn_close).BeginInit();
             SuspendLayout();
             // 
@@ -45,22 +47,48 @@
             btn_close.TabStop = false;
             btn_close.Click += btn_close_Click;
             // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(12, 78);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(776, 410);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // txt_usuarios
+            // 
+            txt_usuarios.BackColor = Color.IndianRed;
+            txt_usuarios.BorderStyle = BorderStyle.FixedSingle;
+            txt_usuarios.Font = new Font("Verdana", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txt_usuarios.Location = new Point(200, 12);
+            txt_usuarios.Name = "txt_usuarios";
+            txt_usuarios.Size = new Size(400, 40);
+            txt_usuarios.TabIndex = 4;
+            txt_usuarios.Text = "Usuários";
+            txt_usuarios.TextAlign = HorizontalAlignment.Center;
+            // 
             // FrmUsuarios
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaptionText;
             ClientSize = new Size(800, 500);
+            Controls.Add(txt_usuarios);
+            Controls.Add(groupBox1);
             Controls.Add(btn_close);
             FormBorderStyle = FormBorderStyle.None;
             Name = "FrmUsuarios";
             Text = "FrmUsuarios";
             ((System.ComponentModel.ISupportInitialize)btn_close).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox btn_close;
+        private GroupBox groupBox1;
+        private TextBox txt_usuarios;
     }
 }

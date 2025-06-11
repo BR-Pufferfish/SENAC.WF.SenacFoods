9bafcec3bbfa4313951dddfb5083cb0fac2e8cef
Puffer Notebook <marceloballinflores@gmail.com>
2025-06-10 21:51:38 -0300

    Criacao das classes do banco de dados

---
+++ SenacFoodsSolucao/SenacFoods/Comanda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenacFoods
+{
+    public class Comanda
+    {
+        public int Id { get; set; }
+        public int NumeroMesa { get; set; }
+        public int Situacao { get; set; }
+        public string NomeCliente { get; set; }
+        public int SituacaoComanda { get; set; }
+
+    }
+}
---
+++ SenacFoodsSolucao/SenacFoods/ComandaDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenacFoods
+{
+    public class ComandaDBContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<CardapioItem> CardapioItems { get; set; }
+        public DbSet<Mesa> Mesas { get; set; }
+        public DbSet<Comanda> Comandas { get; set; }
+
+    }
+}
